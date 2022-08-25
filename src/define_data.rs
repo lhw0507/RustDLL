57ca41ebcc7709bb28117ff8b057acc764bee793
lhw0507 <lhw0507>
2022-08-25 12:50:41 +0900

    중립포맷 json화

--- src/define_data.rs
+++
@@ -1,1 +0,0 @@
-pub mod rebar_specification;
--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,55 @@
+extern crate libc;
+
+use libc::c_char;
+use std::ffi::CStr;
 use std::ffi::CString;
-use define_data::rebar_specification::{RebarSpecification_fromRust, RustRebarSpecInfo};
-mod define_data;
 
 #[no_mangle]
-pub extern "C" fn GetRebarSpecification() -> RebarSpecification_fromRust{
-    let mut name = String::from("KS D 3504");
-    let c_str_name = CString::new(name).unwrap();
-
-    let mut vec = Vec::new();
-
-    let mut firstSpecName = String::from("D4");
-    let c_str_firstSpecName = CString::new(firstSpecName).unwrap();
-
-    vec.push(
-        RustRebarSpecInfo{
-            strName : c_str_firstSpecName.into_raw(),  
-            dDiameter : 0.0042,
-            dBendingRadius : 0.0150,
-            dHookLength_90 : 0.0510,
-            dHookLength_135 : 0.03,
-            dHookLength_180 : 0.06,
-            dWeight : 1.08
-        });
-
-        let mut secondSpecName = String::from("D5");
-        let c_str_secondSpecName = CString::new(secondSpecName).unwrap();
-        vec.push(
-            RustRebarSpecInfo{
-                strName : c_str_secondSpecName.into_raw(),  
-                dDiameter : 0.0053,
-                dBendingRadius : 0.0190,
-                dHookLength_90 : 0.0640,
-                dHookLength_135 : 0.03,
-                dHookLength_180 : 0.06,
-                dWeight : 1.70
-            });
-
-    let boxed_slice: Box<[RustRebarSpecInfo]> = vec.into_boxed_slice();
-    let len = boxed_slice.len();
-    let fat_ptr: *mut [RustRebarSpecInfo] =
-        Box::into_raw(boxed_slice)
-    ;
-    let slim_ptr: *mut RustRebarSpecInfo = fat_ptr as _;
-    RebarSpecification_fromRust{Key : 1, strNamme : c_str_name.into_raw(), specLength : len as i32, ptrSpecInfo : slim_ptr}
+pub extern "C" fn GetRebarSpecification() -> *mut c_char{
+    let json = r#"
+    {
+        "CRebarSpecification": {
+          "Key": 1,
+          "strName": "KS D 3504",
+          "taRebarSpecification": [
+            {
+              "strName" : "D4",  
+              "dDiameter" : 0.0042,
+              "dBendingRadius" : 0.0150,
+              "dHookLength_90" : 0.0510,
+              "dHookLength_135" : 0.03,
+              "dHookLength_180" : 0.06,
+              "dWeight" : 1.08
+            },
+            {
+              "strName" : "D5",  
+              "dDiameter" : 0.0053,
+              "dBendingRadius" : 0.0190,
+              "dHookLength_90" : 0.0640,
+              "dHookLength_135" : 0.03,
+              "dHookLength_180" : 0.06,
+              "dWeight" : 1.70
+            }
+          ]
+        }
+      }
+    "#;
+    let c_str_json = CString::new(json).unwrap();
+    c_str_json.into_raw()
 }
 
+#[no_mangle]
+pub extern "C" fn string_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
+}
 
 #[no_mangle]
-pub extern "C" fn CreateRebarSpecification(input : &RebarSpecification_fromRust) -> bool {
+pub extern "C" fn CreateRebarSpecification(input : *const c_char) -> bool {
     //do something
     true
 }
